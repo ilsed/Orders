6258782ef246406cd527bf79358555e190477967
seppeg <seppe.gielen@cegeka.com>
2017-11-16 11:44:22 +0100

    seppeg: made sure you can have deployable war. But you have to run it from bootrun instead of the application.

--- Order/src/main/java/be/cegeka/orders/order/OrderApplication.java
+++ Order/src/main/java/be/cegeka/orders/order/OrderApplication.java
@@ -2,11 +2,18 @@ package be.cegeka.orders.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class OrderApplication {
+public class OrderApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(OrderApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(OrderApplication.class);
     }
 }
--- Supplier/src/main/java/be/cegeka/orders/supplier/SupplierApplication.java
+++ Supplier/src/main/java/be/cegeka/orders/supplier/SupplierApplication.java
@@ -2,11 +2,18 @@ package be.cegeka.orders.supplier;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SupplierApplication {
+public class SupplierApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(SupplierApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SupplierApplication.class);
     }
 }
